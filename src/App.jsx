0061af5c928791a2304d2477d5fa8bd38685ecbd
Foridul islam <foridulislamdise880@gmail.com>
2026-06-17 11:12:55 +0600

    KFTR_BRAND folder structure setup use vite react tailwind css react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+export default function App() {
+
+  return (
+    <div>
+     <h1>this is a testing time</h1>
+    </div>
+  )
+}
